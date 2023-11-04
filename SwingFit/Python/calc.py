69cfeb9042bb20e25190c471ac48ab7426f85df1
lcael <117420041+lcael@users.noreply.github.com>
2023-11-04 16:10:48 +0900

    좌표 추출 및 계산
    get.py에서 만든 landmark.json을 이용해 필요 좌표를 추출하고 계산 후 result.json으로 저장

---
+++ SwingFit/Python/calc.py
@@ -0,0 +1,23 @@
+import json
+
+# JSON 파일 로드
+json_filename = "landmark.json"  # JSON 파일의 경로와 이름을 지정하세요
+with open(json_filename, 'r') as json_file:
+    data = json.load(json_file)
+
+landmarks = data["Landmarks"]
+midpoints = data["Midpoints"]
+
+head_chest = (midpoints[1]["Y"] - midpoints[0]["Y"]) / (midpoints[1]["X"] - midpoints[0]["X"])
+chest_hip = (midpoints[2]["Y"] - midpoints[1]["Y"]) / (midpoints[2]["X"] - midpoints[1]["X"])
+heel_ham = landmarks[29]["X"] - midpoints[3]["X"]
+elbow_shoulder = landmarks[13]["X"] - landmarks[11]["X"]
+
+result = {"head_chest": head_chest, "chest_hip": chest_hip, "heel-ham": heel_ham, "elbow-shoulder": elbow_shoulder}
+
+    # JSON 파일로 저장
+json_filename = "result.json"
+with open(json_filename, 'w+', encoding='utf-8') as json_file:
+    json.dump(result, json_file, indent=4)
+
+#print(head_chest, chest_hip, heel_ham, elbow_shoulder)
